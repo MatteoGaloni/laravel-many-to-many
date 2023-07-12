0366da7b57559e846793c25959e4632504a5e610
MatteoGaloni <mtgaloni@gmail.com>
2023-07-12 15:46:42 +0200

    add technologies migration

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Psy\CodeCleaner\FunctionReturnInWriteContextPass;
+// use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
